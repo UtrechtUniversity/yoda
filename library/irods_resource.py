aa3b64b4e7ebf66b4da30871dfc5a45754da7bec
Paul Frederiks <p.frederiks@developers.nl>
2017-07-17 09:41:56 +0200

    Able to create resources in a hierarchy

--- library/irods_resource.py
+++ library/irods_resource.py
@@ -13,7 +13,7 @@ IRODSCLIENT_AVAILABLE = False
 try:
     from irods.session import iRODSSession
     from irods.models import Resource
-    from irods.exception import ResourceDoesNotExist
+    from irods.exception import ResourceDoesNotExist, iRODSException
 except:
     pass
 else:
@@ -28,21 +28,30 @@ def get_session():
 
 def main():
     module = AnsibleModule(
-            argument_spec=dict(
-                name=dict(default=None, required=True),
-                location=dict(default=None),
-                vault=dict(default=None, type="path"),
-                children=dict(default=None, type="list"),
-                resourcetype=dict(default=None),
-                state=dict(default="present")
-                ),
-            supports_check_mode=True)
+        argument_spec=dict(
+            name=dict(default=None, required=True),
+            zone=dict(default=None),
+            host=dict(default="EMPTY_RESC_HOST"),
+            vault_path=dict(default="EMPTY_RESC_PATH", type="path"),
+            children=dict(default=None, type="list"),
+            resource_type=dict(default=None),
+            resource_class=dict(default=None),
+            context=dict(default=None),
+            state=dict(default="present")
+            ),
+        supports_check_mode=True)
 
     name = module.params["name"]
-    location = module.params["location"]
-    vault = module.params["vault"]
+    host = module.params["host"]
+    vault_path = module.params["vault_path"]
     children = module.params["children"]
-    resourcetype = module.params["resourcetype"]
+    if children is None:
+        children = []
+
+    resource_type = module.params["resource_type"]
+    resource_class = module.params["resource_class"]
+    context = module.params["context"]
+    state = module.params["state"]
 
     if IRODSCLIENT_AVAILABLE:
         try:
@@ -52,12 +61,40 @@ def main():
     else:
         module.fail_json(msg="python-irodsclient needs to be installed")
 
+
+    changed = False
+
+
     try:
         resource = session.resources.get(name)
     except ResourceDoesNotExist:
-        module.fail_json(msg="Resource {} does not exists".format(name))
+        if state == 'present' and not module.check_mode:
+            resource = session.resources.create(name, resource_type, host=host, path=vault_path, context=context, resource_class=resource_class)
+            changed = True
+        elif state == 'absent':
+            module.exit_json(changed=False, msg="Resource {} is not present".format(name))
+    else:
+        if state == 'absent':
+            module.fail_json(msg="python-irodsclient fails to remove resources in version 0.6")
 
-    module.fail_json(msg="Not implemented yet")
+    for child in children:
+        if resource.children is None or child not in resource.children:
+            try:
+                resource.manager.add_child(name, child)
+            except iRODSException:
+                module.fail_json(msg="iRODSException while adding {} as a child to {}".format(child, name))
+            changed = True
+
+
+    module.exit_json(
+            changed=changed,
+            resource=dict(
+                name=resource.name,
+                zone=resource.zone_name,
+                parent=resource.parent,
+                context=resource.context,
+                status=resource.status),
+            irods_environment=ienv)
 
 from ansible.module_utils.basic import *
 if __name__ == '__main__':
