ee97126651293e9a5180912bb30d1b3d8f1ca154
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-07-20 15:21:12 +0200

    Add Ansible module to edit iRODS environment file.

---
+++ library/irods_environment.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# copyright Utrecht University
+#
+# license: GPL v3
+#
+ANSIBLE_METADATA = {'metadata_version': '1.0',
+                    'status': ['preview'],
+                    'supported_by': 'community'}
+
+
+def main():
+    module = AnsibleModule(
+        argument_spec=dict(
+            path=dict(default=None, required=True),
+            key=dict(default=None, required=True),
+            value=dict(default=None, required=True),
+            state=dict(default="present")
+            ),
+        supports_check_mode=True)
+
+    path = module.params["path"]
+    key = module.params["key"]
+    value = module.params["value"]
+    state = module.params["state"]
+    changed = False
+
+    # Retrieve iRODS environment.
+    with open(path, 'r+') as data_file:
+        irods_environment = json.load(data_file)
+
+        # Set iRODS environment variable.
+        irods_environment[key] = value
+
+        data_file.seek(0)
+        json.dump(irods_environment, data_file, indent=4, sort_keys=True)
+        data_file.truncate()
+        changed = True
+
+    module.exit_json(
+            changed=changed,
+            path=path,
+            key=key,
+            value=value,
+            status=state)
+
+
+from ansible.module_utils.basic import *
+if __name__ == '__main__':
+    main()
