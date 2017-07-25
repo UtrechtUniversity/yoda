5b21b0830da73dd3b4205d3f20ba8fe2b7d7cad0
Lazlo Westerhof <l.r.westerhof@uu.nl>
2017-07-25 11:51:39 +0200

    Possible workaround for permission problem on Samba share.

--- vagrant/provision_controller.sh
+++ vagrant/provision_controller.sh
@@ -8,8 +8,17 @@ if ! command -v ansible >/dev/null; then
     sudo yum install ansible -y
 fi
 
+# Copy SSH key from Samba share to home directory.
+cp /vagrant/vagrant/ssh/vagrant ~/vagrant
+
 # Set file permissions on SSH key to 0600.
-chmod 0600 /vagrant/vagrant/ssh/vagrant
+chmod 0600 ~/vagrant
+
+# Remove key on Samba share.
+sudo rm /vagrant/vagrant/ssh/vagrant
+
+# Create hardlink to SSH key.
+ln ~/vagrant /vagrant/vagrant/ssh/vagrant
 
 # Run YoDa playbook.
 cd /vagrant
