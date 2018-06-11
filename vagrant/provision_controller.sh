ade600845425abbd579e453eb560f8b44d2da3a0
Felix A. Croes <felix.croes@profit4cloud.nl>
2018-06-11 15:37:46 +0200

    Get clean install (not upgrade) of 1.0 to work.

--- vagrant/provision_controller.sh
+++ vagrant/provision_controller.sh
@@ -19,6 +19,7 @@ rm -rf ~/yoda-ansible
 
 # Clone yoda-ansible.
 git clone https://github.com/UtrechtUniversity/yoda-ansible.git
+git checkout release-1.0-tld
 
 # Set file permissions on SSH key to 0600.
 chmod 0600 ~/yoda-ansible/vagrant/ssh/vagrant
