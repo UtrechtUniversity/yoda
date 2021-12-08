55b70915728caad6cae0ef1be8f5ef14ed0263ed
Felix A. Croes <felix@dworkin.nl>
2021-12-08 15:49:28 +0100

    Add indexing plugins to server_config.json.

---
+++ library/irods_elasticsearch.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+# Copyright (c) 2021 Utrecht University
+# GNU General Public License v3.0
+
+ANSIBLE_METADATA = {
+  'supported_by': 'community',
+  'status': ['preview']
+}
+
+from ansible.module_utils.basic import *
+
+
+def main():
+  module = AnsibleModule(
+    argument_spec=dict(
+      config_path=dict(default=None, required=True),
+      state=dict(default="present")
+      ),
+    supports_check_mode=True)
+
+  config_path = module.params["config_path"]
+  state = module.params["state"]
+  changed = False
+
+  # Retrieve iRODS server config.
+  with open(config_path, 'r+') as data_file:
+    server_config = json.load(data_file)
+
+    # Find elasticsearch plugin in server config.
+    found = False
+    plugins = server_config["plugin_configuration"]["rule_engines"]
+    for plugin in plugins:
+      if plugin["plugin_name"] == "irods_rule_engine_plugin-elasticsearch":
+        found = True
+
+    if not found:
+      plugins.extend([
+	{
+          "instance_name": "irods_rule_engine_plugin-indexing-instance",
+          "plugin_name": "irods_rule_engine_plugin-indexing",
+          "plugin_specific_configuration": {}
+        },
+        {
+          "instance_name": "irods_rule_engine_plugin-elasticsearch-instance",
+          "plugin_name": "irods_rule_engine_plugin-elasticsearch",
+          "plugin_specific_configuration": {
+            "hosts" : ["http://localhost:9200/"],
+            "bulk_count" : 100,
+            "read_size" : 4194304
+          }
+        },
+        {
+          "instance_name": "irods_rule_engine_plugin-document_type-instance",
+          "plugin_name": "irods_rule_engine_plugin-document_type",
+          "plugin_specific_configuration": {}
+        }])
+      changed = True
+
+    if not module.check_mode:
+      data_file.seek(0)
+      json.dump(server_config, data_file, indent=4, sort_keys=True)
+      data_file.truncate()
+
+  module.exit_json(
+    changed=changed,
+    config_path=config_path,
+    status=state)
+
+
+if __name__ == '__main__':
+    main()
