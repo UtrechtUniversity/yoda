ee02aa8b51aafbe53d4ef79432d7903afdb5f87c
Sietse Snel <s.t.snel@uu.nl>
2023-04-20 09:53:48 +0200

    Docker cert bundle script: speed up DHParam gen
    Don't generate DHParams file if we already have one, and use
    DSAParam to speed up generation if we don't.

--- docker/cert-bundle/mk-cert-bundle.sh
+++ docker/cert-bundle/mk-cert-bundle.sh
@@ -5,6 +5,11 @@
 set -e
 set -x
 
-openssl req -x509 -newkey rsa:2048 -sha256 -days 3650 -nodes -keyout docker.key -out docker.pem -config docker.cnf
-openssl dhparam -out dhparam.pem 2048
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes -keyout docker.key -out docker.pem -config docker.cnf
+
+if [ -f "dhparam.pem" ]
+then echo "Skipping DHParam generation, because DHParam file is already present."
+else openssl dhparam -dsaparam -out dhparam.pem 4096
+fi
+
 tar cvfz docker-cert-bundle.tar.gz docker.pem docker.key dhparam.pem
