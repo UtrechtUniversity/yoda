d2f2f4b3a8ad074daaa0ad15118db267f6d62a1a
Sietse Snel <s.t.snel@uu.nl>
2023-05-22 13:30:17 +0200

    YDA-5209: Docker setup: configure bind mounts
    Add setup with bind mounts for convenience during development. Old setup
    is kept in compose-without-volumes directory as a fallback.
    
    This change also entails that the container init scripts ensure that the application
    UID matches the UID on the host system, in order to avoid permission problems.

--- docker/images/yoda_eus/yoda_eus_init.sh
+++ docker/images/yoda_eus/yoda_eus_init.sh
@@ -43,6 +43,35 @@ install -m 0644 docker.key /etc/pki/tls/private/localhost.key
 install -m 0644 dhparam.pem /etc/pki/tls/private/dhparams.pem
 progress_update "Certificate data extracted"
 
+CURRENT_UID="$(id -u yodadeployment)"
+if  [[ -f "/var/www/yoda/.docker.gitkeep" || -f "/var/www/extuser/.docker.gitkeep" ]]
+then progress_update "Bind mount detected. Checking if application UID needs to be changed."
+     MOUNT_UID="$(stat -c "%u" /var/www/yoda)"
+     if [ "$MOUNT_UID" == "0" ]
+     then progress_update "Error: bind mount owned by root user. Cannot change application UID. Halting."
+          sleep infinity
+     elif [ "$MOUNT_UID" == "$CURRENT_UID" ]
+     then progress_update "Notice: bind mount UID matches application UID. No need to change application UID."
+     else before_update "Updating application UID ${CURRENT_UID} -> ${MOUNT_UID}"
+          usermod -u "$MOUNT_UID" yodadeployment
+          find / -xdev -user "$CURRENT_UID" -exec chown -h "${MOUNT_UID}" {} \;
+          progress_update "Application UID updated."
+     fi
+     if [[ -d "/var/www/yoda/.git" || -d "/var/www/extuser/yoda-external-user-service/.git" ]]
+     then echo "Git repo detected in bind mounts. Skipping code copy in order not to overwrite local changes."
+     else
+         before_update "Fixing up permissions before copying application source code."
+         find /var/www/yoda-copy -type f -perm 0444 -exec chmod 0666 {} \;
+         find /var/www/extuser-copy -type f -perm 0444 -exec chmod 0666 {} \;
+         progress_update "Permission fixes done."
+         before_update "Copying application source code to volume."
+         cp -Ru /var/www/yoda-copy/. /var/www/yoda
+         cp -Ru /var/www/extuser-copy/. /var/www/extuser
+         progress_update "Copying application source code finished."
+     fi
+else progress_update "Notice: no bind mount detected. Keeping current application UID ${CURRENT_UID}"
+fi
+
 # Download EUS database dump
 before_update "Downloading EUS database dump"
 wget -q "https://yoda.uu.nl/yoda-docker/${DATA_VERSION}.extuser.sql.gz" -O "/download/${DATA_VERSION}.extuser.sql.gz"
