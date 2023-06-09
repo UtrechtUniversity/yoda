4b439a38d91f45a84510c258edf28e954b521628
Sietse Snel <s.t.snel@uu.nl>
2023-06-09 16:55:04 +0200

    Docker setup: change default configuration
    Make the configuration without bind mounts the default one, since it is the
    most portable.
    
    Also expand Docker setup documentation a bit, in particular with regard to troubleshooting.

--- docker/compose-with-bind-mounts/clean.sh
+++ docker/compose-with-bind-mounts/clean.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-./down.sh -v
-for volume in v_extuser_app v_extuser_portal v_irods_logs v_portal_app v_yoda_ruleset
+../down.sh -v
+
+set -e
+
+for volume in $(find . -type d -name "v_*" -maxdepth 1)
 do echo "Removing files on volume $volume ..."
    find "$volume" ! -name '.docker.gitkeep' -type f -exec rm -f {} +
    find "$volume" -type d -mindepth 1 -exec rm -rf {} +
