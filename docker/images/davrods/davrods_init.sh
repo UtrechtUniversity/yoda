94a1db82be6fcc37368e29eb6ad4c1b13b60df0f
Sietse Snel <s.t.snel@uu.nl>
2024-04-23 17:34:26 +0200

    YDA-5660: migrate Docker images to Ubuntu (WIP)

--- docker/images/davrods/davrods_init.sh
+++ docker/images/davrods/davrods_init.sh
@@ -22,8 +22,8 @@ function progress_update {
 }
 
 function start_service {
-  /usr/sbin/httpd -DFOREGROUND || true
-  echo "Error: http either terminated or would not start. Keeping container running for troubleshooting purposes."
+  apache2ctl -D FOREGROUND || true
+  echo "Error: Apache either terminated or would not start. Keeping container running for troubleshooting purposes."
   sleep infinity
 }
 
@@ -41,10 +41,10 @@ progress_update "Downloaded certificate bundle."
 before_update "Extracting certificate data"
 cd /download
 tar xvfz "${DATA_VERSION}.certbundle.tar.gz"
-install -m 0644 docker.pem /etc/pki/tls/certs/localhost.crt
-install -m 0644 docker.pem /etc/pki/tls/certs/localhost_and_chain.crt
-install -m 0644 docker.key /etc/pki/tls/private/localhost.key
-install -m 0644 dhparam.pem /etc/pki/tls/private/dhparams.pem
+install -m 0644 docker.pem /etc/ssl/certs/localhost.crt
+install -m 0644 docker.pem /etc/ssl/certs/localhost_and_chain.crt
+install -m 0644 docker.key /etc/ssl/private/localhost.key
+install -m 0644 dhparam.pem /etc/ssl/private/dhparams.pem
 progress_update "Certificate data extracted"
 
 # Start Apache
